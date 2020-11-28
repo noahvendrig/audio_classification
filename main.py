638da5aab7b13b826ca4b68113dd9929c646ed24
sparkz11 <noahvendrig@gmail.com>
2020-11-28 23:45:23 +1100

    Cleaned

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import librosa.display
 
+# Set the path to the full UrbanSound dataset 
 fulldatasetpath = "D:/Datasets/UrbanSound8K/"
 os.chdir(fulldatasetpath)
 
@@ -26,10 +27,6 @@ def extract_features(file_name):
      
     return mfccsscaled
     
-    
-
-# Set the path to the full UrbanSound dataset 
-#fulldatasetpath = 'D:/Datasets/UrbanSound8K/'
 featuresdf = None
 SAVE_FEATURES = False
 
@@ -107,8 +104,6 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 num_rows = 40
 #num_columns = 174
 
-#num_rows = 55
-#num_columns = 127
 num_channels = 1
 
 #x_train = x_train.reshape(x_train.shape[0], num_rows, num_columns, num_channels)
@@ -130,29 +125,6 @@ model.add(Dropout(0.25))
 
 model.add(Dense(num_labels))
 model.add(Activation('softmax'))
-
-"""
-# Construct model 
-model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=2, input_shape=(num_rows, num_columns, num_channels), activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(filters=32, kernel_size=2, activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(filters=64, kernel_size=2, activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(filters=128, kernel_size=2, activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.2))
-model.add(GlobalAveragePooling2D())
-
-model.add(Dense(num_labels, activation='softmax'))
-"""
 
 # Compile the model
 model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
